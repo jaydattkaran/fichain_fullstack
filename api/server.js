e42fb27bcf854e0367958bd8a8d73d945942ca16
jaydattkaran <karanjaydatt03@gmail.com>
2024-12-12 22:51:25 +0530

    frontend added

--- api/server.js
+++ api/server.js
@@ -23,8 +23,7 @@ const connect = async () => {
         console.error("MongoDB connetction error:", error);
     }
 }
-
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({origin:"https://fichain-frontend.vercel.app", credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
